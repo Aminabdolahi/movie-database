a15741f751d699d7f47963324722c94f0604ad16
aminabdolahi <abdollahi.amin@gmail.com>
2023-07-19 18:06:26 +0330

    image quality problem solved

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -15,7 +15,7 @@ export const MovieCard = ({title, overview, posterPath}:MovieCardProps) => {
   return (
     <div className='bg-white rounded shadow p-4'>
         {/* <img className='w-full h-64 rounded mb-4' src={imageUrl} alt=''/> */}
-        <Image src={imageUrl} width={100} height={100} alt='' className='w-full h-64 rounded mb-4'/>
+        <Image src={imageUrl} width={667} height={943} alt='' className='w-full h-69 rounded mb-4'/>
         <h2 className='text-2xl font-bold mb-2'>{title}</h2>
         <p className='text-sm text-gray-500'>{overview.substring(0,150)}</p>
         
