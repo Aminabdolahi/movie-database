f6084bd082a11ddf96241d7c58919c161bc354ca
aminabdolahi <abdollahi.amin@gmail.com>
2023-07-19 17:21:58 +0330

    movie poster problem solved

--- app/components/MovieSearch.tsx
+++ app/components/MovieSearch.tsx
@@ -17,7 +17,7 @@ const MovieSearch = () => {
         <SearchBar onSearch={handleSearch}/>
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-8'>
         {movies.map((movie:any)=>(
-            <MovieCard key={movie.id} title={movie.title} overview={movie.overview} posterPath={movie.posterPath}/>
+            <MovieCard key={movie.id} title={movie.title} overview={movie.overview} posterPath={movie.poster_path}/>
         ))}
     </div>
     </div>
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -20,7 +20,9 @@ export const SearchBar = ({onSearch} :SearchBarProps) => {
   return (
     <form onSubmit={handleSubmit} className=' flex items-center w-screen max-w-3xl'>
         <input type='text' className='border border-gray-300 rounded-1 py-2 px-4 w-full focus:outline-none focus:border-blue-500' value={query} onChange={handleChange}/>
-        <button type='submit' className='bg-red-600'/>
+        <button type='submit' className='bg-red-600 text-white py-2 px-6 rounded-r hover:bg-blue-600 focus:outline-none'>
+            Search
+        </button>
     </form>
   )
 }
